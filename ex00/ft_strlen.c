d71466fb78e61888bce6e3bc76e6556225d6b4d9
Mike Pourbaix <mike@pourbaix.be>
2022-09-29 13:30:45 +0200

    Premier dépot

---
+++ ex00/ft_strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_strlen(char *str);
+
+int main()
+{
+	char str[] = "Bonjour";
+
+	printf("%d", strlen(str));
+
+	return 0;
+}
---
+++ ex01/ft_putstr.c
@@ -0,0 +1,14 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+void ft_putstr(char *str);
+
+int main()
+{
+	char *str[10];
+
+	*str = "bonjour";
+
+	puts(char str[1]);
+}
